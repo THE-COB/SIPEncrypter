18fa8fa0e67002f28192b3440f5b4ad5f64d6d06
THE-COB <jmlife6@gmail.com>
2017-02-27 17:24:35 -0500

    added js to turn string into decimal

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+var string = prompt("Type in your string");
+var stringLength = string.length;
+var letters = [];
+for(var i = 0; i<stringLength; i++){
+	letters.push(string.charCodeAt(i))
+}
+letters = letters.join("%");
+
+alert(letters);
+
+
 
