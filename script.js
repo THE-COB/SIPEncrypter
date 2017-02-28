0499c542a3f88f5c9701af40eca9fbbb48406a79
THE-COB <jmlife6@gmail.com>
2017-02-28 17:00:27 -0500

    STRING TURNED INTO BINARY ARRAY!

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
 // Use this function to initialize the algorithm
 var submit = function(){
-	//Pulling User Input from HTML page
+
 	var userString = document.getElementById("userString").value
 	var userLink = document.getElementById("userImage").value
 	var userImage = new Image();
 	userImage.src = userLink;
-
-
 }
-
-
-
-
 
 var string = prompt("Type in your string");
 var stringLength = string.length;
 var letters = [];
 for(var i = 0; i<stringLength; i++){
-	letters.push(string.charCodeAt(i))
+	letters.push(string.charCodeAt(i).toString(2))
 }
-letters = letters.join("%");
 
 alert(letters);
-
-
-
