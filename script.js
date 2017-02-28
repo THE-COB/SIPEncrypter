33eeead59d4b91d5525233816ff8b4815ffe70f6
nullifies <drew.ripberger@gmail.com>
2017-02-28 09:51:49 -0500

    Added JS reference to image and variables

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Use this function to initialize the algorithm
+var submit = function(){
+	//Pulling User Input from HTML page
+	var userString = document.getElementById("userString").value
+	var userLink = document.getElementById("userImage").value
+	var userImage = new Image();
+	userImage.src = userLink;
+
+
+}
+
+
+
+
+
 var string = prompt("Type in your string");
 var stringLength = string.length;
 var letters = [];
