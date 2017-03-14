82aa1e2c5e8b7afcf14896ee4335f8c8ca15f1c8
nullifies <drew.ripberger@gmail.com>
2017-03-14 09:52:44 -0400

    Made a usable image and removed prompt

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 // Use this function to initialize the algorithm
 var submit = function(){
 
-	var userString = document.getElementById("userString").value
-	var userLink = document.getElementById("userImage").value
+	// Making a usable image to manipulate from the user's link
+	var userString = document.getElementById("userString").value;
+	var userLink = document.getElementById("userImage").value;
 	var userImage = new Image();
 	userImage.src = userLink;
-}
+	var canvas = document.getElementById("finalImage");
+	var ctx = canvas.getContext("2d");
 
-var string = prompt("Type in your string");
-var stringLength = string.length;
-var letters = [];
-for(var i = 0; i<stringLength; i++){
-	letters.push(string.charCodeAt(i).toString(2))
-}
+	userImage.onload = function(){
+		ctx.drawImage(userImage, 0, 0);
+	}
 
-alert(letters);
+	// Converting the user's string to binary
+	var stringLength = userString.length;
+	var letters = [];
+	for(var i = 0; i<stringLength; i++){
+		letters.push(userString.charCodeAt(i).toString(2));
+	}
+	
+}
